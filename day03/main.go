b81853df7b59d32ae27f7b91105a843595c2c577
Matthew Barrington <matthew@barrington.it>
2022-12-03 18:35:10 +0000

    Clean up the code a bit

--- day03/main.go
+++ day03/main.go
@@ -18,8 +18,20 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 
-	fmt.Println("part 1:", dupes(lines))
-	fmt.Println("part 2:", badges(lines))
+	dupe, badge := complete(lines)
+	fmt.Println("part 1:", dupe)
+	fmt.Println("part 2:", badge)
+}
+
+func complete(items []string) (rune, rune) {
+	var dupe, badge rune
+
+	for i := 0; i < len(items); i += 3 {
+		dupe += dupes(items[i : i+3])
+		badge += badges(items[i : i+3])
+	}
+
+	return dupe, badge
 }
 
 func dupes(items []string) rune {
@@ -30,7 +42,7 @@ func dupes(items []string) rune {
 		first := stuff[:len(stuff)/2]
 		second := stuff[len(stuff)/2:]
 
-		tmp, _ := doubleIntersection(first, second)
+		tmp := doubleIntersection(first, second)
 		num += numberRep([]rune(tmp[0])[0])
 	}
 	return num
@@ -43,7 +55,7 @@ func badges(items []string) rune {
 		two := strings.Split(items[i+1], "")
 		three := strings.Split(items[i+2], "")
 
-		tmp, _ := tripleIntersection(one, two, three)
+		tmp := tripleIntersection(one, two, three)
 
 		num += numberRep([]rune(tmp[0])[0])
 	}
@@ -51,28 +63,28 @@ func badges(items []string) rune {
 	return num
 }
 
-func doubleIntersection(first, second []string) ([]string, bool) {
+func doubleIntersection(first, second []string) []string {
 	for _, x := range first {
 		for _, y := range second {
 			if x == y {
-				return []string{x}, true
+				return []string{x}
 			}
 		}
 	}
-	return []string{}, false
+	return []string{}
 }
 
-func tripleIntersection(first, second, third []string) ([]string, bool) {
+func tripleIntersection(first, second, third []string) []string {
 	for _, x := range first {
 		for _, y := range second {
 			for _, z := range third {
 				if x == y && y == z {
-					return []string{x}, true
+					return []string{x}
 				}
 			}
 		}
 	}
-	return []string{}, false
+	return []string{}
 }
 
 func numberRep(r rune) rune {
