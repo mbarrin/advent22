4f35e19967d3d360d3b77078678b8705e2664f5c
Matthew Barrington <matthew@barrington.it>
2022-12-10 15:22:14 +0000

    day 10 formatting

--- day10/main.go
+++ day10/main.go
@@ -27,6 +27,7 @@ func main() {
 		if (tick+20)%40 == 0 {
 			total += (register * tick)
 		}
+
 		rowID := (tick - 1) / 40
 		colID := (tick - 1) % 40
 		if colID == register-1 || colID == register || colID == register+1 {
@@ -42,12 +43,13 @@ func main() {
 			if (tick+20)%40 == 0 {
 				total += (register * tick)
 			}
+
 			rowID := (tick - 1) / 40
 			colID := (tick - 1) % 40
-
 			if colID == register-1 || colID == register || colID == register+1 {
 				screen[rowID][colID] = "#"
 			}
+
 			register += num
 		}
 		tick++
@@ -55,6 +57,7 @@ func main() {
 
 	fmt.Println("part 1:", total)
 
+	fmt.Println("part 2:")
 	for _, row := range screen {
 		fmt.Println(strings.Join(row, ""))
 	}
